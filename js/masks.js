7e1021e44c84f6c73d4ce683b5a1d57316eeec5f
DigitalJS24 <digitaljs@gmail.com>
2020-11-04 11:05:55 +0200

    add scroll down option on about and projects section

--- js/masks.js
+++ js/masks.js
@@ -2,3 +2,9 @@ $(function() {
     //задание заполнителя с помощью параметра placeholder
     $("#callback-form-input-phone").mask("+380 (99) 999-99-99");
 });
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
+    });
+});
